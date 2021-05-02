27427033e77ed1f373bae2ab7a645f93c96f9e04
Siddhant Thakur <43845363+DevSidd@users.noreply.github.com>
2021-05-02 14:48:54 +0530

    Update CourseServiceImpl.java

--- src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
+++ src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -15,37 +15,20 @@ public class CourseServiceImpl implements CourseService {
 	
 	@Autowired
 	private CourseDao courseDao;
-	
-//	List<Course> list;
-	
+		
 	public CourseServiceImpl() {
-		
-//		list=new ArrayList<>();
-//		list.add(new Course(145, "Java Core Courses", "this course contain basics to advance of java"));
-//		list.add(new Course(4343, "Spring Boot Courses", "creating rest api using spring boot"));
-		
+				
 	}
 
 	@Override
 	public List<Course> getCourses() {
-		// return list;
 		
 		return courseDao.findAll();
+		
 	}
 
 	@Override
 	public Course getCourse(long courseId) {
-		
-//		Course c = null;
-//		for(Course course : list)
-//		{
-//			if(course.getId() == courseId)
-//			{
-//				c=course;
-//				break;
-//			}
-//		}
-	//	return c;
 		
 		return courseDao.getOne(courseId);
 		
@@ -54,8 +37,6 @@ public class CourseServiceImpl implements CourseService {
 	@Override
 	public Course addCourse(Course course) {
 		
-	//	list.add(course);
-		
 		courseDao.save(course);
 		return course;
 		
@@ -63,23 +44,15 @@ public class CourseServiceImpl implements CourseService {
 
 	@Override
 	public Course updateCourse(Course course) {
-		
-//		list.forEach(e -> {
-//			if (e.getId() == course.getId()) {
-//				e.setTitle(course.getTitle());
-//				e.setDescription(course.getDescription());
-//			}
-//		});
-		
+				
 		courseDao.save(course);
 		return course;
+		
 	}
 
 	@Override
 	public void deleteCourse(long parseLong) {
-		
-	//	list = this.list.stream().filter(e->e.getId()!=parseLong).collect(Collectors.toList());
-		
+			
 		Course entity = courseDao.getOne(parseLong);
 		courseDao.delete(entity);
 		
